1ef5af8fdb26df464dfcf0a965549dcf1946795d
Tom Smith <tom.smith@qunifi.com>
2024-03-04 10:58:35 +0000

    use hash routing

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router/auto'
+import { createRouter, createWebHashHistory } from 'vue-router/auto'
 import { VueQueryPlugin } from '@tanstack/vue-query'
 import App from './App.vue'
 
@@ -10,7 +10,7 @@ import './styles/style.css'
 const app = createApp(App)
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
 })
 
 app.use(router)
